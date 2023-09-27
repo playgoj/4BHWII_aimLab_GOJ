2e623895b561c11aa1c4b310bfef52f834975f81
buchaaaaa <137984904+playgoj@users.noreply.github.com>
2023-09-27 14:36:48 +0200

    Movment jump etc

--- 4BHWII_aimLab_GOJ/Assets/Player/Player_Script.cs
+++ 4BHWII_aimLab_GOJ/Assets/Player/Player_Script.cs
@@ -2,23 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Player_Script : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
 
-    public float speed = 0.5f;
+    public float speed;
+    public float jumpHeight = 300;
+    public float mouseXSpeed = 1;
+    Rigidbody rigid;
+    
     void Update()
     {
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
+        float mouseXinput = Input.GetAxis("Mouse X");
 
-        transform.position += speed*transform.forward * verticalInput;
-        transform.position += speed*transform.right * horizontalInput;
+        transform.Rotate(0, mouseXinput*mouseXSpeed, 0);
+
+        transform.position += transform.forward * verticalInput * speed;
+        transform.position += transform.right * horizontalInput * speed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.AddForce(Vector3.up * jumpHeight);
+        }
     }
 }
